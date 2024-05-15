bcb0a80ce13e20eb695c4edcb525e6494db8ecb9
Nikolay Mutafchiev <niki_mutafchiev@yahoo.com>
2024-05-15 20:00:58 +0300

    docker-compose for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />}></Route>
           <Route path="/tasks" element={<TaskManager tasks={[{ name: "Task1", due: "01-01-2001", description: "Presentation for english class" }, { name: "Task2", due: "04-04-2004" }, { name: "Task3", due: "06-06-2006" }]} />} ></Route>
-
         </Routes>
       </Router>
     </>
