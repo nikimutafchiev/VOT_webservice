0f08afbd4788773fc1597cd29a38268ba065aee2
Nikolay Mutafchiev <niki_mutafchiev@yahoo.com>
2024-05-31 17:33:25 +0300

    MariaDB docker-compose

--- backend/main.py
+++ backend/main.py
@@ -9,8 +9,13 @@ CORS(app)
 
 app.secret_key = b'hjokasi62783767'
 host = os.getenv("MYSQL_HOST",default="localhost")
-database = mysql.connector.connect(host = host, user = "user2@localhost", password = "User2!User2!", database="TaskManager")
+database = mysql.connector.connect(host = host, user = "maxscale'@'192.168.0.%", password = "my_s3cret")
 cursor = database.cursor(dictionary=True)
+cursor.execute("CREATE DATABASE TaskManager")
+database.database = "TaskManager"
+
+cursor.execute("CREATE TABLE Users(id int PRIMARY KEY AUTO_INCREMENT,name varchar(255),password varchar(255));")
+cursor.execute("CREATE TABLE Tasks(id int PRIMARY KEY AUTO_INCREMENT,name varchar(255),description varchar(255),due date,user_id int not null,FOREIGN KEY(user_id) REFERENCES Users(id));")
 
 @app.route("/my_tasks")
 def index():
