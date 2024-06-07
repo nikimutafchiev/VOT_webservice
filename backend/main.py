6ed0be9c09d0fd96ec94c2e5cb0c283d9a657391
Nikolay Mutafchiev <niki_mutafchiev@yahoo.com>
2024-06-07 14:49:29 +0300

    maxscale

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
 import mysql.connector
-import socket
-import os
+
 
 app = Flask(__name__)
 CORS(app)
 
 app.secret_key = b'hjokasi62783767'
-host = os.getenv("MYSQL_HOST",default="localhost")
-database = mysql.connector.connect(host = host, user = "maxscale'@'192.168.0.%", password = "my_s3cret")
-cursor = database.cursor(dictionary=True)
+
+connection = mysql.connector.connect(host = "maxscale", user = "maxscale", password = "my_s3cret",port=4006)
+cursor = connection.cursor(dictionary=True)
+
 cursor.execute("CREATE DATABASE TaskManager")
-database.database = "TaskManager"
+connection.database = "TaskManager"
 
 cursor.execute("CREATE TABLE Users(id int PRIMARY KEY AUTO_INCREMENT,name varchar(255),password varchar(255));")
 cursor.execute("CREATE TABLE Tasks(id int PRIMARY KEY AUTO_INCREMENT,name varchar(255),description varchar(255),due date,user_id int not null,FOREIGN KEY(user_id) REFERENCES Users(id));")
